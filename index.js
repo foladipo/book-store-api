009baebb34eceb80db8797fc59de4fd727f93052
Folusho Oladipo <folusho.oladipo@andela.com>
2018-10-29 17:01:34 +0100

    Run app on a port determined by environment values.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const app = require("./src/app");
+const dotenv = require("dotenv");
 
-app.listen(5001, () => {
-    console.log("Node API server is running on port 5001 now.");
+dotenv.config();
+
+const DEFAULT_PORT = 3001;
+const PORT = process.env.PORT || DEFAULT_PORT;
+
+app.listen(PORT, () => {
+    console.log(`Node API server is running on port ${PORT} now.`);
 });
