825a1ac062028d775646d47c6653f2e5d1892694
Folusho Oladipo <folusho.oladipo@andela.com>
2018-11-01 14:35:57 +0100

    Add new columns and column constraints to a migration.

--- src/migrations/01-Users.js
+++ src/migrations/01-Users.js
@@ -1,28 +1,39 @@
 module.exports = {
     up: (queryInterface, Sequelize) => {
         return queryInterface.createTable("Users", {
+            id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                autoIncrement: true,
+                unique: true,
+                primaryKey: true
+            },
             firstName: {
-                type: Sequelize.STRING
+                type: Sequelize.STRING,
+                allowNull: false,
             },
             lastName: {
-                type: Sequelize.STRING
+                type: Sequelize.STRING,
+                allowNull: false
             },
             username: {
-                type: Sequelize.STRING
+                type: Sequelize.STRING,
+                allowNull: false,
+                unique: true
             },
             password: {
-                type: Sequelize.STRING
+                type: Sequelize.STRING,
+                allowNull: false
             },
             createdAt: {
-                type: Sequelize.DATE
+                type: Sequelize.DATE,
+                allowNull: false
             },
             updatedAt: {
+                allowNull: false,
                 type: Sequelize.DATE
             }
-        }, {
-                freezeTableName: true
-            }
-        );
+        });
     },
 
     down: (queryInterface) => {
