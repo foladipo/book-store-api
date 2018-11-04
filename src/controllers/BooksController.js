55ea9075eefa5b08834855d9fe561fce7cd7c7a9
Folusho Oladipo <folusho.oladipo@andela.com>
2018-11-04 07:53:44 +0100

    Enable users to get all public books.

--- src/controllers/BooksController.js
+++ src/controllers/BooksController.js
@@ -40,4 +40,35 @@ export default class BooksController {
                 });
             });
     }
+
+    /**
+     * @name getAllBooks
+     * @method getAllBooks
+     * @description A controller used to fetch all the public books stored
+     * in the app.
+     * @param {Request} req - data about the request sent to this controller.
+     * @param {Response} res - the response from this controller.
+     * @returns {void}
+     */
+    static getAllBooks(req, res) {
+        Books
+            .findAll({
+                attributes: ["id", "title", "author", "publicationDate", "genres"],
+                where: {
+                    isPrivate: false
+                }
+            })
+            .then(allBooks => {
+                res.status(200).json({
+                    message: "Successfully fetched all books.",
+                    books: allBooks
+                });
+            })
+            .catch(() => {
+                res.status(500).json({
+                    message: "An error occurred while fetching all books. Please check your request and try again.",
+                    error: "FailedFetchAllBooksError"
+                });
+            });
+    }
 }
--- src/routes/booksRoute.js
+++ src/routes/booksRoute.js
@@ -6,5 +6,6 @@ import validateNewBook from "../middleware/validateNewBook";
 
 const booksRoute = express.Router();
 booksRoute.route("/addNewBook").post(validateToken, validateNewBook, BooksController.addNewBook);
+booksRoute.route("/").get(validateToken, BooksController.getAllBooks);
 
 export default booksRoute;
