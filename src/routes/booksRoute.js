bc361cc5e943025ccd960a393b2ece6192be4cc0
Folusho Oladipo <folusho.oladipo@andela.com>
2019-06-24 12:48:32 +0100

    Update routes and controllers.

--- src/routes/booksRoute.js
+++ src/routes/booksRoute.js
@@ -6,8 +6,9 @@ import validateNewBook from "../middleware/validateNewBook";
 import validateBookUpdate from "../middleware/validateBookUpdate";
 
 const booksRoute = express.Router();
-booksRoute.route("/addNewBook").post(validateToken, validateNewBook, BooksController.addNewBook);
-booksRoute.route("/").get(validateToken, BooksController.getAllBooks);
+booksRoute.route("/")
+    .get(validateToken, BooksController.getAllBooks)
+    .post(validateToken, validateNewBook, BooksController.addNewBook);
 booksRoute.route("/:id")
     .get(validateToken, BooksController.getBook)
     .put(validateToken, validateBookUpdate, BooksController.updateBook)
